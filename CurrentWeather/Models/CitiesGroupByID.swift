e5032badd8078da6b9cb84fe66dd0eeb536de914
rusellkhx <rusellkh87@gmail.com>
2020-09-02 13:12:20 +0500

    add "timeZone" for struct <CitiesGroupByID> need for correct time HH:mm

--- CurrentWeather/Models/CitiesGroupByID.swift
+++ CurrentWeather/Models/CitiesGroupByID.swift
@@ -10,25 +10,26 @@ struct ListForDecodableJSONcitiesGroupByID {
     var dt: Int
     var temperature: String
     var name: String
+    var timeZone: Int
     
     var dtStr: String {
-        return datemsToString(from: dt)
+        return datemsToString(from: dt, timeZone: timeZone)
     }
     
-    init(dt: Int, temperature: String, name: String) {
+    init(dt: Int, temperature: String, name: String, timeZone: Int) {
         self.dt = dt
         self.temperature = temperature
         self.name = name
+        self.timeZone = timeZone
     }
     
-    private func datemsToString(from date: Int) -> String {
+    private func datemsToString(from date: Int, timeZone: Int) -> String {
         let date = Date(timeIntervalSince1970: Double(date))
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "E, d MMM HH:mm"
-        //dateFormatter.timeZone = .current
+        dateFormatter.timeZone =  TimeZone(secondsFromGMT: timeZone)
         return dateFormatter.string(from: date)
     }
-
 }
 
 import Foundation
@@ -36,15 +37,15 @@ import Foundation
 // MARK: - CitiesGroupByID
 struct CitiesGroupByID {
     let cnt: Int
-    
     let list: [ListForDecodableJSONcitiesGroupByID]
-
+    
     init?(citiesGroupByID: ClockByDayWeatherData2) {
         var list2 = [ListForDecodableJSONcitiesGroupByID]()
         for city in 0..<citiesGroupByID.list.count{
             list2.append(ListForDecodableJSONcitiesGroupByID(dt: citiesGroupByID.list[city].dt,
                                                              temperature: String(format: "%.0f", citiesGroupByID.list[city].main.temp),
-                                                             name: citiesGroupByID.list[city].name))
+                                                             name: citiesGroupByID.list[city].name,
+                                                             timeZone: citiesGroupByID.list[city].sys.timezone))
         }
         self.list = list2
         self.cnt = citiesGroupByID.cnt
